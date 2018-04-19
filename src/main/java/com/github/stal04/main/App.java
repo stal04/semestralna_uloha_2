9d4f5e85a89692f7fe7a673c29f0f99c34b62fe1
Lenka Šťastná <lenka2277@gmail.com>
2018-04-19 22:40:54 +0200

    prvý commit

---
+++ src/main/java/com/github/stal04/main/App.java
@@ -0,0 +1,13 @@
+package com.github.stal04.main;
+
+/**
+ * Hello world! juchuu
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
